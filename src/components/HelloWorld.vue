529ef4ee7110ceb1af07f8df54a7c5da5e68f3a1
Raynal R. Gobel <raynal.gobel@gmail.com>
2018-07-13 08:22:48 +0700

    STEP-1: Create initial layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="container">
+    <div class="columns">
+      <div class="column col-3">
+
+        <div class="card">
+          <div class="card-header">
+            <div class="card-title h5">ProductName</div>
+            <div class="card-subtitle text-gray">product description goes here</div>
+          </div>
+          <div class="card-image">
+            <img src="http://via.placeholder.com/350x150" alt="card-image" class="img-responsive">
+          </div>
+          <div class="card-body">
+            <h4>Rp. 10,900</h4>
+          </div>
+          <div class="card-footer">
+            <div class="btn-group">
+              <button class="btn btn-primary">Add to Cart</button>
+              <button class="btn">Share</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="column col-3">
+        Content
+      </div>
+      <div class="column col-3">
+        Content
+      </div>
+      <div class="column col-3">
+        Content
+      </div>
+    </div>
   </div>
 </template>
 
@@ -93,21 +46,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
