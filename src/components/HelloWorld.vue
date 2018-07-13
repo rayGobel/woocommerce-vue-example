691c063680a4b60664f9c85b76c5db8d9f477751
Raynal R. Gobel <raynal.gobel@gmail.com>
2018-07-13 10:24:42 +0700

    STEP-1: Insert component layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,17 +5,25 @@
         <product-card :product="product"></product-card>
       </div>
     </div>
+    <div class="divider"></div>
+    <div class="columns">
+      <div class="column col-12">
+        <pagination></pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ProductService from '@/api/ProductService'
 import ProductCard from '@/components/ProductCard'
+import Pagination from '@/components/Pagination'
 
 export default {
   name: 'HelloWorld',
   components: {
-    'product-card': ProductCard
+    'product-card': ProductCard,
+    'pagination': Pagination
   },
   data () {
     return {
---
+++ src/components/Pagination.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="centered">
+    <ul class="pagination">
+      <li class="page-item disabled">
+        <a href="#" tabindex="-1">Previous</a>
+      </li>
+      <li class="page-item active">
+        <a href="#">1</a>
+      </li>
+      <li class="page-item">
+        <a href="#">2</a>
+      </li>
+      <li class="page-item">
+        <a href="#">3</a>
+      </li>
+      <li class="page-item">
+        <span>...</span>
+      </li>
+      <li class="page-item">
+        <a href="#">12</a>
+      </li>
+      <li class="page-item">
+        <a href="#">Next</a>
+      </li>
+    </ul>
+  </div>
+</template>
