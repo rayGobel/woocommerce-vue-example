9e9803d6549c216c8ca8ab6d84ac1126f96f4cfb
Raynal R. Gobel <raynal.gobel@gmail.com>
2018-07-13 10:06:42 +0700

    STEP-2: Create components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,43 +2,29 @@
   <div class="container">
     <div class="columns">
       <div class="column col-3">
-
-        <div class="card">
-          <div class="card-header">
-            <div class="card-title h5">ProductName</div>
-            <div class="card-subtitle text-gray">product description goes here</div>
-          </div>
-          <div class="card-image">
-            <img src="http://via.placeholder.com/350x150" alt="card-image" class="img-responsive">
-          </div>
-          <div class="card-body">
-            <h4>Rp. 10,900</h4>
-          </div>
-          <div class="card-footer">
-            <div class="btn-group">
-              <button class="btn btn-primary">Add to Cart</button>
-              <button class="btn">Share</button>
-            </div>
-          </div>
-        </div>
-
+        <product-card></product-card>
       </div>
       <div class="column col-3">
-        Content
+        <product-card></product-card>
       </div>
       <div class="column col-3">
-        Content
+        <product-card></product-card>
       </div>
       <div class="column col-3">
-        Content
+        <product-card></product-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    'product-card': ProductCard
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <div class="card-title h5">ProductName</div>
+      <div class="card-subtitle text-gray">product description goes here</div>
+    </div>
+    <div class="card-image">
+      <img src="http://via.placeholder.com/350x150" alt="card-image" class="img-responsive">
+    </div>
+    <div class="card-body">
+      <h4>Rp. 10,900</h4>
+    </div>
+    <div class="card-footer">
+      <div class="btn-group">
+        <button class="btn btn-primary">Add to Cart</button>
+        <button class="btn">Share</button>
+      </div>
+    </div>
+  </div>
+</template>
